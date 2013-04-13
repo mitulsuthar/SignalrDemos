908a9e5e6a75e264be8fec56a7e86c7d9ec407d9
Mitul Suthar <notareal[removethis]email@yahoo.com>
2013-04-13 03:24:18 -0400

    Added SignalR Demos to source control
    First working demo of Signalr Self Host using Owin and JavaScript Client
    with No Proxies.

---
+++ SignalRConsoleCSharp/SignalRConsoleCSharp/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRConsoleCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://localhost:8080";
+
+            using (WebApplication.Start<Startup>(url))
+            {
+                Console.WriteLine("Server running on {0}", url);
+                Console.ReadLine();
+            }
+        }
+    }
+
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // Turn cross domain on 
+            var config = new HubConfiguration { EnableCrossDomain = true };
+
+            // This will map out to http://localhost:8080/signalr by default
+            app.MapHubs(config);
+        }
+    }
+    [HubName("myHub")]
+    public class MyHub : Hub
+    {
+        public void chat(string message)
+        {
+            Clients.All.addMessage(message);
+        }
+    }
+}
